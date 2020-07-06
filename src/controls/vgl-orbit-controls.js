2404683dc7cb4f85152ab1dc262bd290a2acc94a
僵硬小香蕉 <linguochi@cmiot.chinamobile.com>
2020-07-06 14:20:45 +0800

    add vgl-orbit-controls.js

---
+++ src/controls/vgl-orbit-controls.js
@@ -0,0 +1,25 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+export default {
+  inject: ['vglNamespace'],
+  name: 'VglOrbitControls',
+  props: ['camera'],
+  computed: {
+    cmr() {
+      return this.vglNamespace.cameras.hash.c;
+    },
+  },
+  watch: {
+    cmr: {
+      handler(cmr) {
+        this.nextTick(() => {
+          const controls = new OrbitControls(cmr, this.$parent.$el);
+          controls.addEventListener('change', () => {
+            this.$parent.requestRender();
+          });
+        });
+      },
+      immediate: true,
+    },
+  },
+};
